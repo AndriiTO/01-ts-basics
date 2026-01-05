1c40a9b252b43bdd6a6372b27db7ca8478c9fb42
seedre <taocripto@gmail.com>
2026-01-05 21:46:17 +0100

    first try

---
+++ src/task-4.ts
@@ -0,0 +1,16 @@
+type PrintUserInfo = (
+  name: string,
+  age: number,
+  email?: string
+) => void;
+
+const printUserInfo: PrintUserInfo = (name, age, email) => {
+  console.log("Name:", name);
+  console.log("Age:", age);
+  if (email) {
+    console.log("Email:", email);
+  }
+};
+
+printUserInfo("Alice", 30);
+printUserInfo("Bob", 25, "bob@mail.com");
